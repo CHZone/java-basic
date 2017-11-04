b86570f93b368eabe77738e7293ad2f8e6ffcc71
CHZone <546312949@qq.coom>
2017-11-04 19:51:11 +0800

    process RuntimeDemo.java

--- src/main/java/basic/process/simple/RumtimeDemo.java
+++ src/main/java/basic/process/simple/RumtimeDemo.java
@@ -1,7 +1,7 @@
 package basic.process.simple;
 
-import java.io.IOException;
-import java.util.Scanner;
+        import java.io.IOException;
+        import java.util.Scanner;
 
 public class RumtimeDemo {
     public static void main(String[] args){
